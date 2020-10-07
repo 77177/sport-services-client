d926c82cabc89b53d79fe3da30fac7c5d53ad112
Stas Tarasov <iluvstrwrs@mail.ru>
2020-10-07 14:21:37 +0300

    refactoring and added service

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TestService} from '../../service/test/test.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private testService: TestService) { }
 
   ngOnInit(): void {
+  }
+
+  test(): string {
+    return this.testService.test();
   }
 
 }
