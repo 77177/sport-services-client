dfb1e343bb001fc2ce636866af7dddcc43756492
Stas Tarasov <iluvstrwrs@mail.ru>
2020-10-18 19:15:02 +0300

    added room list

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TestService} from '../../test.service';
+import {Room} from '../objects/room';
 
 @Component({
   selector: 'app-rooms',
@@ -9,12 +10,18 @@ import {TestService} from '../../test.service';
 export class RoomsComponent implements OnInit {
 
   public testObject: string;
+  public rooms: Room[];
 
   constructor(private testService: TestService) { }
 
   ngOnInit(): void {
     this.testService.test()
       .subscribe(value => this.testObject = value.test);
+    this.testService.getRooms()
+      .subscribe(value => {
+        console.log(value);
+        this.rooms = value;
+      });
   }
 
 }
--- src/app/test.service.ts
+++ src/app/test.service.ts
@@ -2,16 +2,23 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Test} from './components/objects/test';
+import {Room} from './components/objects/room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
 
+  private url = 'http://localhost:8080';
+
   constructor(private httpClient: HttpClient) { }
 
 
   test(): Observable<Test>{
-    return this.httpClient.get<Test>('http://localhost:8080/v1/api/test');
+    return this.httpClient.get<Test>(this.url + '/v1/api/test');
+  }
+
+  getRooms(): Observable<Room[]> {
+    return this.httpClient.get<Room[]>(this.url + '/v1/api/room/all');
   }
 }
