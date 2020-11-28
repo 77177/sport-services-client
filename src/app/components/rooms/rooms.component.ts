d7478173d3a899df22e1b8311aa3a39da283a465
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-28 19:25:07 +0300

    room request sending

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -12,12 +12,33 @@ export class RoomsComponent implements OnInit {
   public testObject: string;
   public sign = 'ROOMS';
   public rooms: Room[];
+  public roomRequest = {
+    roomId: -1,
+    trainerId: -1,
+    start: '',
+    end: ''
+  };
 
   constructor(private testService: TestService) { }
 
   ngOnInit(): void {
     this.testService.getRooms()
       .subscribe(value => this.rooms = value);
+  }
+
+  getUser(): any {
+    return this.testService.getUser();
+  }
+
+  isTrainer(): boolean {
+    return this.getUser().authority === 'ROLE_TRAINER';
+  }
+
+  createRoomRequest(roomId: number, trainerId: any): void {
+    console.log('working');
+    this.roomRequest.roomId = roomId;
+    this.roomRequest.trainerId = trainerId;
+    console.log(this.roomRequest);
   }
 
 }
